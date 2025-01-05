a4d0d84323a1f1c4b96752b5a85fa5952e191ffc
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-05 13:18:36 +0330

    search result item

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
 
@@ -8,9 +8,28 @@ function Hotels() {
 
   const destination = searchParams.get("destination");
   const room = JSON.parse(searchParams.get("options"))?.room;
-  const { isLoading, data } = useFetch("http://localhost:5000/hotels", `q=${destination || ""}&accommodates_gte=${room || 1}`);
+  const { isLoading, data } = useFetch("http://localhost:5000/hotels", `q=${destination || ""}&accommodates_gte=${room || 1}`); // q ==> search in all data with q
   if (isLoading) return <Loader />;
-  return <div>{data.length}</div>;
+  return (
+    <div className="searchList">
+      <h2>Search Result ({data.length})</h2>
+      {data.map((item) => {
+        return (
+          <Link key={item.id} to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}>
+            <div className="searchItem">
+              <img src={item.thumbnail_url} alt={item.name} />
+              <div className="searchItemDesc">
+                <p className="location">{item.smart_location}</p>
+                <p className="name">{item.name}</p>
+                €&nbsp;{item.price}&nbsp;
+                <span>night</span>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Hotels;
