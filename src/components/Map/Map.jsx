81434cc137bf50ee04394bae1c513201d091e03a
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-06 22:17:13 +0330

    sync map center with lat and lng

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHotels } from "../context/HotelsProvider";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const { isLoading, hotels } = useHotels();
-  const [mapCenter, setMapCenter] = useState([51, 3]);
+  const [mapCenter, setMapCenter] = useState([50, 4]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  // get lat&lng and save in useState with useEffect
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
   return (
     <div className="mapContainer">
       <MapContainer className="map" center={mapCenter} zoom={13} scrollWheelZoom={true}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+        <ChangeCenter position={mapCenter} />
         {hotels.map((item) => {
           return (
             <Marker key={item.id} position={[item.latitude, item.longitude]}>
@@ -23,3 +33,10 @@ function Map() {
 }
 
 export default Map;
+
+//use function change location with click on single hotels
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
