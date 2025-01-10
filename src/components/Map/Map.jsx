ea298e52e308021a178f65e1cab0814ffb500d1e
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-01-10 08:24:44 +0330

    Add New Bookmark

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import useGeoLocation from "../../hooks/useGeoLocation";
+import useUrlLocation from "../../hooks/useUrlLocation";
 
 function Map({ markerLocation }) {
   const [mapCenter, setMapCenter] = useState([50, 4]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { lat, lng } = useUrlLocation();
 
   const { isLoading: isLoadingPosiotion, position: geoLocationPosition, getPosition } = useGeoLocation();
 
